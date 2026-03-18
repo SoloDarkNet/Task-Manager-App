29288974e0eee7b8c8fe0a1e30f156f4dd1f5d64
Solomon <solomonzabath@gmail.com>
2026-03-18 17:34:49 +0530

    URl changed

--- Client/TaskManager/src/Services/app.jsx
+++ Client/TaskManager/src/Services/app.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-app-1-2t4z.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
