177a077e6432d3c522a55e4156b14f979d762401
SoloDarkNet <solomonzabath@gmail.com>
2026-06-24 19:29:22 +0530

    Update aiRoutes.js

--- Server/Routes/aiRoutes.js
+++ Server/Routes/aiRoutes.js
@@ -3,5 +3,11 @@ const Router = express.Router();
 const { getAISuggestions } = require("../Controller/aiController");
 
 Router.post("/suggestions", getAISuggestions);
+Router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "AI Route Working",
+  });
+});
 
 module.exports = Router;
